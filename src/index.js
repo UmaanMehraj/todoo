e269b376ae3e38e9e17cd878997dd061b9825566
UmaanMehraj <umaanmehraj5@gmail.com>
2026-03-19 19:28:10 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css"
+import {Task} from "./tasks.js"
+
+const obj1 =new Task("Hw", "I have to do maths h/w", "12-08-2026", 1 , "Blah Blah Blah")
+console.log(obj1)
+
+console.log('Hey Logged!')
