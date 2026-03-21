ded42a7d41bea5810f3546e8c78629265b67e5d2
UmaanMehraj <umaanmehraj5@gmail.com>
2026-03-21 17:03:58 +0530

    styling

--- src/tasks.js
+++ src/tasks.js
@@ -1,7 +1,7 @@
 export class Project{
-    constructor(name, id){
+    constructor(name){
         this.name = name
-        this.id = id
+        this.id = self.crypto.randomUUID()
         this.toDos= []
     }
 
