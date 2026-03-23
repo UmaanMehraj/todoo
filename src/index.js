3206a8879ef8752976b2509bdc319d07c05a686f
UmaanMehraj <umaanmehraj5@gmail.com>
2026-03-23 10:25:52 +0530

    styling, js testing

--- src/index.js
+++ src/index.js
@@ -9,14 +9,28 @@ p1.appendToDos(t1)
 p1.appendToDos(t2)
 
 const addProjectBtn = document.querySelector('#addProject')
+const projectConfirmBtn = document.querySelector('#projectConfirmBtn')
 const projectDialog = document.querySelector('.projectDialog')
 addProjectBtn.addEventListener('click', ()=>{
     projectDialog.showModal()
 })
 
+
 const addTaskBtn = document.querySelector('#addTask')
 const taskDialog = document.querySelector('.taskDialog')
+const taskConfirmBtn = document.querySelector('#taskConfirmBtn')
 addTaskBtn.addEventListener('click', ()=>{
     taskDialog.showModal()
 })
+taskConfirmBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    taskDialog.close()
+})
+
+projectConfirmBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    projectDialog.close()
+})
+
+
 console.log(p1)
