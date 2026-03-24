868ae9c90830b329324ab27e199a938efaf4e7f6
UmaanMehraj <umaanmehraj5@gmail.com>
2026-03-24 22:27:34 +0530

    add: display function

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -1,4 +1,9 @@
-export function appendProject(Project){
+import viewImage from "./img/view.png"
+import deleteImage from "./img/delete.png"
+import editImage from "./img/delete.png"
+
+
+export function appendProject(Project) {
     const content = document.querySelector('.content')
     const newDiv = document.createElement('div')
     const btn = document.createElement('button')
@@ -9,6 +14,25 @@ export function appendProject(Project){
     content.append(newDiv)
 }
 
-export function appendTask(Project){
-    
+export function displayTask(Project) {
+    Project.todos.array.forEach(task => {
+        const parentDiv = document.createElement('div')
+
+        const nameDiv = document.createElement('div')
+        nameDiv.setAttribute('class', 'name')
+        nameDiv.innerText = task.name
+
+        const btnDiv = document.createElement('div')
+        btnDiv.setAttribute('class', 'btns')
+
+        const viewBtn = document.createElement('button')
+        const viewImg = document.createElement('img')
+        viewImg.src = viewImage
+        viewBtn.append(viewImg)
+
+        btnDiv.append(viewBtn)
+
+        
+
+    });
 }
