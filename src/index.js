9eebf6274e638cc5a445a71c06d5cbac8fefed24
UmaanMehraj <umaanmehraj5@gmail.com>
2026-03-28 09:59:04 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -1,56 +1,51 @@
-import "./styles.css";
-import { Task, Project } from "./tasks.js";
-import { projects, appendProject, displayTask } from "./domFunctions.js";
+import './styles.css';
+import { Task, Project } from './tasks.js';
+import { projects, appendProject, displayTask } from './domFunctions.js';
 
-const addProjectBtn = document.querySelector("#addProject");
-const projectConfirmBtn = document.querySelector("#projectConfirmBtn");
-const projectDialog = document.querySelector(".projectDialog");
-const projectName = document.querySelector("#name");
-addProjectBtn.addEventListener("click", () => {
+const addProjectBtn = document.querySelector('#addProject');
+const projectConfirmBtn = document.querySelector('#projectConfirmBtn');
+const projectDialog = document.querySelector('.projectDialog');
+const projectName = document.querySelector('#name');
+addProjectBtn.addEventListener('click', () => {
   projectDialog.showModal();
 });
-projectConfirmBtn.addEventListener("click", (e) => {
+projectConfirmBtn.addEventListener('click', (e) => {
   e.preventDefault();
   projectDialog.close();
 });
 
-
-projectDialog.addEventListener("close", () => {
-   const project = new Project(projectName.value);
-  if (project.name !== "") {
+projectDialog.addEventListener('close', () => {
+  const project = new Project(projectName.value);
+  if (project.name !== '') {
     appendProject(project);
     console.log(projects);
-    return project
+    return project;
   }
 });
 
+const addTaskBtn = document.querySelector('#addTask');
+const taskDialog = document.querySelector('.taskDialog');
+const taskConfirmBtn = document.querySelector('#taskConfirmBtn');
 
-const addTaskBtn = document.querySelector("#addTask");
-const taskDialog = document.querySelector(".taskDialog");
-const taskConfirmBtn = document.querySelector("#taskConfirmBtn");
+const tasktTitle = document.querySelector('#title');
+const taskDesc = document.querySelector('#description');
+const taskDate = document.querySelector('#date');
+const taskNotes = document.querySelector('#notes');
+const taskPriority = document.querySelector('#priority');
 
-const tasktTitle = document.querySelector("#title");
-const taskDesc = document.querySelector("#description");
-const taskDate = document.querySelector("#date");
-const taskNotes = document.querySelector("#notes");
-const taskPriority = document.querySelector("#priority");
-
-addTaskBtn.addEventListener("click", () => {
+addTaskBtn.addEventListener('click', () => {
   taskDialog.showModal();
 });
-taskDialog.addEventListener("close", () => {
-  const task = new Task(
-    tasktTitle.value,
-    taskDesc.value,
-    taskDate.value,
-    taskPriority.value,
-    taskNotes.value,
-  );
-  if (task) {
-   displayTask(task)
-  }
+taskDialog.addEventListener('close', () => {
+    const task = new Task(
+      tasktTitle.value,
+      taskDesc.value,
+      taskDate.value,
+      taskPriority.value,
+      taskNotes.value,
+    );
 });
-taskConfirmBtn.addEventListener("click", (e) => {
+taskConfirmBtn.addEventListener('click', (e) => {
   e.preventDefault();
   taskDialog.close();
 });
